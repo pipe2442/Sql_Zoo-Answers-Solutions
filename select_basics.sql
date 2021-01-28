4e8de6ec760739c075a3c79a84d2ee5d286978d7
pipe2442 <felipeangulo2442@gmail.com>
2021-01-28 15:48:12 -0500

    Add basics fie

---
+++ select_basics.sql
@@ -0,0 +1,11 @@
+/* 1 problem */
+SELECT population FROM world
+  WHERE name = 'Germany'
+
+/* 2 problem */
+SELECT name, population FROM world
+  WHERE name IN ('Sweden', 'Norway', 'Denmark');
+
+/* 3 problem */
+SELECT name, area FROM world
+  WHERE area BETWEEN 200000 AND 250000
